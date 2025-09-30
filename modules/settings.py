9df5fd14c2b32390442c8a1bff2d247b0528912c
Theboys23456 <krishna93107437@gmail.com>
2025-09-30 13:56:28 +0530

    Update settings.py

--- modules/settings.py
+++ modules/settings.py
@@ -40,7 +40,7 @@ def register_settings_handlers(bot):
         ])
         await callback_query.message.edit_media(
         InputMediaPhoto(
-          media="https://tinypic.host/images/2025/07/14/file_00000000fc2461fbbdd6bc500cecbff8_conversation_id6874702c-9760-800e-b0bf-8e0bcf8a3833message_id964012ce-7ef5-4ad4-88e0-1c41ed240c03-1-1.jpg",
+          media="https://i.ibb.co/7xm7cXyg/STRANGER-BOY.jpg",
           caption=caption
         ),
         reply_markup=keyboard
@@ -57,7 +57,7 @@ def register_settings_handlers(bot):
         ])
         await callback_query.message.edit_media(
         InputMediaPhoto(
-          media="https://tinypic.host/images/2025/07/14/file_00000000fc2461fbbdd6bc500cecbff8_conversation_id6874702c-9760-800e-b0bf-8e0bcf8a3833message_id964012ce-7ef5-4ad4-88e0-1c41ed240c03-1-1.jpg",
+          media="https://i.ibb.co/ZRvCxd5g/STRANGER-BOY.jpg",
           caption=caption
         ),
         reply_markup=keyboard
@@ -75,7 +75,7 @@ def register_settings_handlers(bot):
         ])
         await callback_query.message.edit_media(
         InputMediaPhoto(
-          media="https://tinypic.host/images/2025/07/14/file_00000000fc2461fbbdd6bc500cecbff8_conversation_id6874702c-9760-800e-b0bf-8e0bcf8a3833message_id964012ce-7ef5-4ad4-88e0-1c41ed240c03-1-1.jpg",
+          media="https://i.ibb.co/ccV44ZRS/STRANGER-BOY.jpge0-1c41ed240c03-1-1.jpg",
           caption=caption
         ),
         reply_markup=keyboard
